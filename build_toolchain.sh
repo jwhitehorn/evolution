8ebdbeff556c0a1157e2e3bfa73c463854586bde
Jason Whitehorn <jason.whitehorn@gmail.com>
2011-08-18 00:21:07 -0500

    attempt at adding libc, and a hello world

--- build_toolchain.sh
+++ build_toolchain.sh
@@ -8,6 +8,7 @@ export TARGET=i386-apple-darwin9.0.0
 #brew install mpc
 
 mkdir toolchain
+cd ./toolchain
 mkdir src
 cd ./src
 if [ ! -f gcc-4.6.1.tar.gz ];
@@ -15,12 +16,21 @@ then
 wget http://ftp.gnu.org/gnu/gcc/gcc-4.6.1/gcc-4.6.1.tar.gz -O gcc-4.6.1.tar.gz
 fi
 tar -xvzf gcc-4.6.1.tar.gz
+
 if [ ! -f binutils-2.21.1.tar.gz ];
 then
 wget http://ftp.gnu.org/gnu/binutils/binutils-2.21.1.tar.gz -O binutils-2.21.1.tar.gz
 fi
 tar -xvzf binutils-2.21.1.tar.gz
-mkdir build-binutils build-gcc
+
+if [ ! -f libc-763.11.tar.gz ];
+then
+wget http://www.opensource.apple.com/tarballs/Libc/Libc-763.11.tar.gz libc-763.11.tar.gz
+fi
+tar -xvzf libc-763.11.tar.gz
+
+
+mkdir build-binutils build-gcc build-libc
 
 cd build-binutils
 ../binutils-2.21.1/configure --target=$TARGET --prefix=$PREFIX --disable-nls
@@ -32,3 +42,6 @@ export PATH=$PATH:$PREFIX/bin
 ../gcc-4.6.1/configure --target=$TARGET --prefix=$PREFIX --disable-nls --without-headers
 make all
 make install-gcc
+
+cd ../build-libc
+#../Libc-763.11/configure --target=$TARGET --prefix=$PREFIX
